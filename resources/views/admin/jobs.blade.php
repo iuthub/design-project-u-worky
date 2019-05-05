ca764d3c5028d40a1f9dc68c7b67333c4a63ddf1
Azamat Vakhobov <azamat9812@gmail.com>
2019-05-05 14:05:49 +0500

    added google places api

--- resources/views/admin/jobs.blade.php
+++ resources/views/admin/jobs.blade.php
@@ -39,7 +39,8 @@
                             Job category
                         </div>
                         <div class="col-xl-9 col-lg-9">
-                            <select class="mdb-select md-form colorful-select dropdown-primary" name="category_id" required>
+                            <select class="mdb-select md-form colorful-select dropdown-primary" name="category_id"
+                                required>
                                 <option value="" disabled selected></option>
                                 <option value="1">IT &amp; Computer</option>
                                 <option value="2">Finance &amp; Logistics</option>
@@ -104,8 +105,8 @@
                         </div>
                         <div class="col-xl-5 col-lg-5">
                             <div class="md-form">
-                                <input type="number" placeholder="To..." name="salary_id_to" value="" class="form-control"
-                                    required>
+                                <input type="number" placeholder="To..." name="salary_id_to" value=""
+                                    class="form-control" required>
                             </div>
                         </div>
                     </div>
@@ -136,24 +137,30 @@
                         </div>
                     </div>
 
-
                     <div class="row form-item">
                         <div class="col-xl-3 col-lg-3 form-item-title">
                             Address
                         </div>
                         <div class="col-xl-9 col-lg-9">
                             <div class="md-form">
-                                <input type="text" id="location" name="location_id" value="" class="form-control">
+                                <input type="hidden" name="location-name" id="location-name" />
+                                <input type="hidden" name="lat" id="lat" />
+                                <input type="hidden" name="lng" id="lng" />
+                                <input type="text" id="search" name="search" value="" class="form-control"
+                                    oninput="onSearch(this.value)">
                             </div>
+                            <select class="mdb-select md-form colorful-select dropdown-primary" id="results"
+                                onchange="setMarker(this.value)">
+                                <option value="" disabled selected>Location...</option>
+                            </select>
                         </div>
                     </div>
 
 
                     <!-- MAP -->
                     <div class="row form-item">
-                        <div class="col-12 my-2">
-                            <div id="map" class="map-container" style="height: 360px">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.2474156061717!2d69.24356531518035!3d41.32523300780929!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b76b266e1e9%3A0xa6f5e1f04d9e6e1c!2sTuron+Telecom!5e0!3m2!1sen!2s!4v1555309567930!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        <div class="col-12">
+                            <div id="map" class="map-container">
                             </div>
                         </div>
                     </div>
@@ -174,13 +181,72 @@
     </div>
     @endsection
 
-
     @section('footer')
     @include('admin.partials.footer')
     @endsection
 
     @section('scripts')
     <script type="text/javascript" src="{{asset('frontend/js/vendor/tinymce/tinymce.min.js')}}"></script>
+    <script type="text/javascript"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_EikIUgqB3NMPmITWXgHd9XOYEpBPnrw&callback=initMap&libraries=places"
+        async defer> </script>
+    <script>
+        let map, service;
+            let values;
+            const TASHKENT = {lat: 41.311081, lng: 69.240562};
+            function initMap() {
+                map = new google.maps.Map(document.getElementById('map'), {
+                    center: {lat: 41.311081, lng: 69.240562},
+                    zoom: 13
+                });
+
+                service =  new google.maps.places.PlacesService(map);
+            }
+
+            function onSearch(val) {
+                if (val.length == 0) return;
+                const request = {
+                    query: val,
+                    location: TASHKENT,
+                    radius: 15000,
+                    fields: ["name", "geometry"]
+                }
+
+                service.textSearch(request, (results, status) => {
+                    $("#results").empty();
+                    values = results;
+
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        results.forEach(element => {
+                            let text = $(`
+                                <option value="${element.id}">${element.name}</option>
+                            `)
+
+                            $("#results").append(text);
+                        });
+                    }
+            });
+            }
+            function setMarker(val) {
+                console.log(val);
+                values.forEach(element => {
+                    if (element.id == val) {
+                        console.log(element);
+                        let marker = new google.maps.Marker({
+                            position: new google.maps.LatLng({lat: element.geometry.location.lat(), lng: element.geometry.location.lng()}),
+                            animation: google.maps.Animation.DROP,
+                            map: map
+                        });
+                        map.setCenter(element.geometry.location);
+
+                        $("#location-name").val(element.formatted_address);
+                        $("#lat").val(element.geometry.location.lat());
+                        $("#lng").val(element.geometry.location.lng());
+                    }
+                });
+            }
+    </script>
+
     <script>
         $(document).ready(function() {
             $("#publishBtn").click(function() {
