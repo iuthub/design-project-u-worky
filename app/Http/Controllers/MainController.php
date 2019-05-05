53e1d873b970b80e85a6c7175fe2523f0d2afa89
Davron Islamov <idavron.u@gmail.com>
2019-05-05 20:19:21 +0500

    Bug fixes

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -32,7 +32,6 @@ class MainController extends Controller {
 
     public function postJobs(Request $request){
         $data = $request->input();
-        $jobs = Job::where
         return view('dashboard.jobs');
     }
 }
